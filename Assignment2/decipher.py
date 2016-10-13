cf9a186b811e000e5ecde99af4b8f9cad717bf9f
Nicolas ANGELARD-GONTIER <nicolas.angelard-gontier@mail.mcgill.ca>
2016-10-13 13:11:34 -0400

    code refactoring

--- Assignment2/decipher.py
+++ Assignment2/decipher.py
@@ -9,6 +9,7 @@ import argparse
 
 from nltk.tag import hmm
 
+
 ################################################################################
 # reading data set
 def read_cipher(cipher_folder):
@@ -23,69 +24,79 @@ def read_cipher(cipher_folder):
     file_names = ['train_cipher.txt', 'train_plain.txt', 'test_cipher.txt', 'test_plain.txt']
 
     for (file_name, i) in zip(file_names, range(4)):
-        file = open(os.path.join(cipher_folder, file_name))
-        for line in file:
+        text_file = open(os.path.join(cipher_folder, file_name))
+        for line in text_file:
             data[i].append(line)
 
     return data
 
+
 ################################################################################
 # returning sequence of labels
-def get_sequences(X, Y):
+def get_sequences(ciphered_text, plain_text):
     """
     Make a list of labeled sequences for each sentence in X and Y.
-    :param X: array of strings representing 1 line of observable items.
-    :param Y: array of strings representing 1 line of hidden tags.
+    :param ciphered_text: array of strings representing 1 line of observable items.
+    :param plain_text: array of strings representing 1 line of hidden tags.
     :return: list of the form (sequence_for_sentence_1, ..., sequence_for_sentence_n) where each
             sequence is a list of 2-tuple (item, tag) for each character in that specific sentence.
     """
     sequences = []
 
-    for sentence_o, sentence_h in zip(X, Y):
-        # for each sentence in X and Y,
-        #  list("sentence") = ['s',...,'e']
-        # append a list of tuple ('char i in sentence_o', 'char i in sentence_h')
+    for sentence_o, sentence_h in zip(ciphered_text, plain_text):
+        # for each sentence in X and Y:
+        #  append a list of tuple ('char i in sentence_o', 'char i in sentence_h')
         #  where 1 tuple = 1 character.
+        # list("sentence") = ['s',...,'e']
         sequences.append(zip(list(sentence_o), list(sentence_h)))
 
     return sequences
 
 
-parser = argparse.ArgumentParser(description="Decipher some text using HMM.")
-parser.add_argument("cipher_folder",
-                    help="path to the folder containing the cipher and plain text files")
-parser.add_argument("-laplace",
-                    action="store_true",
-                    help="turn on laplace smoothing")
-parser.add_argument("-lm",
-                    action="store_true",
-                    help="turn on the language model from assignment1")
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description="Decipher some text using HMM.")
+    parser.add_argument("cipher_folder",
+                        help="path to the folder containing the cipher and plain text files")
+    parser.add_argument("-laplace",
+                        action="store_true",
+                        help="turn on laplace smoothing")
+    parser.add_argument("-lm",
+                        action="store_true",
+                        help="turn on the language model from assignment1")
+    args = parser.parse_args()
+
+    [x_train, y_train, x_test, y_test] = read_cipher(args.cipher_folder)
+    # each item is of the form: ["sentence 1", "sentence 2", ..., "sentence n"]
+    # print "x_train"; print x_train
+    # print "y_train"; print y_train
+    # print "x_test"; print x_test
+    # print "y_test"; print y_test
+
+    print "laplace:", args.laplace
+    print "lm:", args.lm
+
+    ##
+    # Training
+    ##
+    labeled_sequences = get_sequences(x_train, y_train)  # of the form [ [('#','#')...], [...], ...]
+
+    trainer = hmm.HiddenMarkovModelTrainer()
+    tagger = trainer.train_supervised(labeled_sequences)  # estimator = None -> MLE is default
+
+    ##
+    # Prediction
+    ##
+    for i in range(len(x_test)):  # for each sentence in test set
+        print "cipher:\n", x_test[i]
+        print "plain:\n", y_test[i]
+        # predict the real characters based on the list of ciphered characters:
+        predictions = tagger.best_path(list(x_test[i]))  # get array of predicted characters
+        print "prediction:\n", ''.join(predictions)
+        print "- - -"
+
+    test_sequences = get_sequences(x_test, y_test)  # of the form [ [('#','#')...], [...], ...]
+    tagger.test(test_sequences)
 
 
-[X_train, Y_train, X_test, Y_test] = read_cipher(args.cipher_folder)
-# each item is of the form: ["sentence 1", "sentence 2", ..., "sentence n"]
-#print "X_train"; print X_train
-#print "Y_train"; print Y_train
-#print "X_test"; print X_test
-#print "Y_test"; print Y_test
-
-print "laplace:", args.laplace
-print "lm:", args.lm
-
-labeled_sequences = get_sequences(X_train, Y_train)  # of the form [ [('#','#')...], [...], ...]
-
-trainer = hmm.HiddenMarkovModelTrainer()
-tagger = trainer.train_supervised(labeled_sequences)  # estimator = None -> MLE is default
-
-for i in range(len(X_test)):  # for each sentence in test set
-    print "cipher:"; print X_test[i]
-    print "plain:"; print Y_test[i]
-    # predict the real characters based on the list of ciphered characters:
-    predictions = tagger.best_path(list(X_test[i]))  # get array of predicted characters
-    print "prediction:"; print ''.join(predictions)
-    print "- - -"
-
-test_sequences = get_sequences(X_test, Y_test)  # of the form [ [('#','#')...], [...], ...]
-tagger.test(test_sequences)
-
+if __name__ == '__main__':
+    main()
